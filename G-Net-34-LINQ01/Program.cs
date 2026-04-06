e219425e5db17fc5427a1aaf25e3bbaf94ecb578
Nada Hamdy <nadaahasabbalh@gmail.com>
2026-04-06 22:20:39 +0200

    q1

--- G-Net-34-LINQ01/Program.cs
+++ G-Net-34-LINQ01/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
+            #region 1
             Console.WriteLine("Hello, World!");
+            List<Product> ProductList = ListGenerators.ProductList;
+
+            // 1.Get all products from the "Seafood" category.Print each product's name and price.
+            var seafoodproducts = ProductList.Where(p => p.category == "seafood");
+            foreach(var product in seafoodproducts)
+            {
+                Console.WriteLine($"Name: {product.ProductName}, Price: {product.UnitPrice}");
+
+            }
+            #endregion
         }
-    }
-} 
+    } 
